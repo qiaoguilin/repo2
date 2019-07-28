cac15c07ab13fc597aefb22ff1ad14d5090a4067
qb <123@con.ithiema>
2019-07-28 18:27:22 +0800

    上传文件

---
+++ src/com/itheima/FIrstClass.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class FIrstClass {
+    public static void main(String[] args) {
+        System.out.println("hello itheima");
+        System.out.println("hello itheima");
+        System.out.println("hello itheima");
+        System.out.println("hello itheima");
+    }
+}
